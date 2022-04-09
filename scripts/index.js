6d56823330cd36ceefb3b3934aaafe9d116d48da
Andrew Monis <5andrewmonis7@gmail.com>
2022-04-09 13:11:10 -0400

    JS finished / not cleaned up yet

--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,12 @@ const closeMenu = document.querySelector("#close-menu");
 const selectionBtn = document.querySelectorAll(".selection-btn");
 const radioBtn = document.querySelectorAll(".radio-btn");
 const yourPledge = document.querySelectorAll(".your-pledge");
+const success = document.querySelector("#success");
+const next = document.querySelectorAll(".continue");
+const finished = document.querySelector("#finished");
+const innerBar = document.querySelector(".innerbar");
+const textSelect = document.querySelector(".text-select");
+const bookmark = document.querySelectorAll("#bookmark");
 
 // Nav Menu
 hamburger.addEventListener("click", menu);
@@ -65,10 +71,6 @@ function selectionMenuOpen(e) {
     const target = e.target;
     const selected = target.getAttribute("aria-controls")
 
-    const tabContainer = target.parentNode;
-    const mainContainer = tabContainer.parentNode;
-
-
     selectRadio(`#${selected}`)
 }
 
@@ -80,6 +82,7 @@ function selectRadio(content){
     const mainContainer = secondContainer.parentNode;
 
     mainContainer.querySelector('.enter-pledge').setAttribute("data-visible", true)
+    pledgedAmount = mainContainer.querySelector('.amount').innerHTML
 
 }
 
@@ -91,6 +94,7 @@ radioBtn.forEach(e => {
         const mainContainer = secondContainer.parentNode;
 
         mainContainer.querySelector('.enter-pledge').setAttribute("data-visible", true)
+        pledgedAmount = mainContainer.querySelector('.amount').innerHTML
 
     })
 })
@@ -101,11 +105,88 @@ yourPledge.forEach(e => {
     e.addEventListener("keyup", changePledge)
 })
 
+let pledgedAmount = 0;
+let changedAmount = 0;
+
 function changePledge() {
     const parent = document.querySelector('[data-visible="true"]');
     const value = parent.querySelector("input").value;
-    const amount = parent.querySelector("#amount")
+    const amount = parent.querySelector(".amount")
 
     amount.innerHTML = value;
 
+    if(value === ""){
+        amount.innerHTML = pledgedAmount;
+        changedAmount = parseInt(pledgedAmount);
+    }else{
+        amount.innerHTML = value;
+        changedAmount = parseInt(value);
+    }
+    
+
 }
+
+
+// Updated numbers
+
+const moneyBacked = document.querySelector("#money-backed");
+const totalBackers = document.querySelector("#total-backers");
+
+let totalMoney = 89914
+let backers = 5007
+
+function updateNumbers(){
+    moneyBacked.innerHTML = '$' + totalMoney.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    totalBackers.innerHTML = backers.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+    if(totalMoney.toString().length === 5){
+        innerBar.style.width = `${totalMoney.toString().slice(0, 2)}%`;
+    }else{
+        innerBar.style.width = `${totalMoney.toString().slice(0, 3)}%`;
+    }
+
+    
+}
+
+// Success 
+
+next.forEach(e => {
+    e.addEventListener("click", thankYou);
+})
+
+function thankYou() {
+    selectionMenu()
+
+    window.scrollTo(0, 0)
+    success.style.display = "flex";
+
+    backers++
+    if(changedAmount === 0){
+        totalMoney += parseInt(pledgedAmount)
+    }else{
+        totalMoney += parseInt(changedAmount)
+    }
+
+    updateNumbers()
+
+    const parent = document.querySelector('[data-visible="true"]');
+    parent.querySelector("input").value = "";
+    
+}
+
+finished.addEventListener("click", closeThankYou)
+
+function closeThankYou() {
+    success.style.display = "none";
+}
+
+
+// Create bookmark
+bookmark.addEventListener("click", createBookmark);
+
+let createBookmark = browser.bookmarks.create({
+    title: "crowdfunding",
+    url: "https://developer.mozilla.org/Add-ons/WebExtensions/API/bookmarks/create"
+  });
+
+
